c9ae6bcbdf5fbdfd09b95483d6b7de7e823c0a8c
iostream232 <fernandoiostream@gmail.com>
2024-06-23 09:57:59 -0600

    Se agrega el proyecto

---
+++ reglasDeVARIABLES.js
@@ -0,0 +1,52 @@
+//indetificadores de nombre de varibles
+
+//-Reglas para nombres de variables 
+
+/*
+
+Debe de comenzar con:
+
+#Una letra de (A-Z) o (a -z)
+#Un signo de dolar ($)
+#Con un guion bajo (_)
+
+los caracteres  siguientes pueden ser: 
+
+
+*Letras (A-z o a-z)
+*Digitos (0 - 9)
+*guiones bajos (_)
+*signos de dolar ($)
+
+Ej. let nombre;  o let miVariable1;
+
+Java Script utiliza la notacion de altas y bajas para los nombre de las 
+variables.
+
+camel-case
+miVariable
+
+*/
+
+let miPrimeraVariable;
+let miNombre;
+let ErroAutenticacion;
+let CalificacionFinal;
+let camelCase;
+let EstaEsLaNotacionDelCamello;
+
+//Reglas de nombres de variables 
+//letras , $,_, posteriormente se pueden usar numeros
+
+let $miNumero1 = 10;
+
+console.log($miNumero1);
+
+let _miNumero2 = 20;
+
+
+//Recomendable utilizar en java Scrip
+
+let miNumero3 =30;
+console.log(miNumero3)
+
---
+++ variables.js
@@ -0,0 +1,5 @@
+var miNumero1 = 15;
+console.log(miNumero1)
+
+let miNumero = 20;
+console.log(miNumero)
